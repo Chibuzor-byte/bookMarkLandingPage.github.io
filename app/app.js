39a0345bbea4905136e2e4370ed9809cc767e98f
osinachi-codes <chibuzorosinachi21@gmail.com>
2022-10-01 14:07:13 +0100

    third commit

--- app/app.js
+++ app/app.js
@@ -1,5 +1,21 @@
-const clickMe = document.querySelector('.click');
 
-clickMe.addEventListener('click', function(){
-    console.log('I love you');
-})
+const btns = document.querySelectorAll(".click");
+
+
+btns.forEach((el) => el.addEventListener("click", function(){
+
+    const faqParagraph = this.nextElementSibling;
+
+    // const arrow = document.querySelector(".icon-arrow");
+    const arrow = this.childNodes[3].childNodes[0]
+    
+    if (faqParagraph.style.maxHeight){
+        faqParagraph.style.maxHeight = null;
+        arrow.src = "././images/icon-arrow-blue.svg";
+        arrow.style.transform = 'rotate(0deg)';
+    } else {
+        arrow.src = "././images/icon-arrow-red.svg";
+        arrow.style.transform = 'rotate(180deg)';
+        faqParagraph.style.maxHeight = faqParagraph.scrollHeight + "px";
+    }
+}));
